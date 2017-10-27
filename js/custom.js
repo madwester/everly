c2630b0264eb462ef40b77b156da7506faf45676
Madeleine Westerstrom <madwester@gmail.com>
2017-10-27 12:25:07 +1100

    dsds

--- js/custom.js
+++ js/custom.js
@@ -23,4 +23,21 @@ var onMapClickHandler = function (event) {
     }
     // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
-//$(document).ready(function () {
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("go-up-btn").style.display = "block";
+    }
+    else {
+        document.getElementById("go-up-btn").style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
